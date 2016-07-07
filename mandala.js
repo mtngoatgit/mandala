740ed988f02f22263f47dda451142535536caf9d
Paxton Bigler <paxton.bigler@gmail.com>
2016-07-07 14:59:32 -0600

    beginning manipulations with jQuery

---
+++ mandala.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+
+
+$('.cel').hover(function (){
+    $('.cel').addClass('orangeD');
+}, function() {
+    $('.cel').removeClass('orangeD');
+})
+
+$('.ter').hover(function (){
+    $('.ter').addClass('orangeM');
+}, function() {
+    $('.ter').removeClass('orangeM');
+})
+
+$('.tel').hover(function (){
+    $('.tel').addClass('orangeL');
+}, function() {
+    $('.tel').removeClass('orangeL');
+})
+
+
+// $('#header #menu li a').hover(function () {
+//   $(this).fadeOut(0).addClass('hover').fadeIn(300);
+// },
+// funct
+
+// (
+//   function() {
+//     $( this ).addClass( "hover" );
+//   }, function() {
+//     $( this ).removeClass( "hover" );
+//   }
+// );
+
+// $('#menu li a').hover(function(){
+//     $(this).fadeOut('fast', function(){
+//         $(this).addClass('hover').fadeIn(300);
+//     });
+// }, function(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
